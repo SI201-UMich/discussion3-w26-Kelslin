e03421eae4576d2a9e90ac4bef71429226adc120
Kelsey Lin <yliiiiiiiin11@gmail.com>
2026-02-04 17:38:34 -0500

    Completed all the code and tested with more rectangle objects to ensure we are comparing both width, length and area of the rectangles when using eq equation

--- Discussion3.py
+++ Discussion3.py
@@ -14,7 +14,7 @@ class Rectangle():
 
     # YOUR CODE HERE
     def __str__(self):
-        return f"Width: {self.width}, Length: {self.height}"
+        return f"Width: {self.width}, Length: {self.length}"
 
 
     # Create the "area_calculator" method
@@ -28,8 +28,8 @@ class Rectangle():
     # Returns a boolean value
 
     # YOUR CODE HERE
-    def __eq__(self):
-        if
+    def __eq__(self, other):
+        return self.area_calculator() == other.area_calculator() and self.width == other.width and self.length == other.length
 
     
 
@@ -40,6 +40,7 @@ def main():
     print(r1)
     # call the area_calculator method
     print("Area:", r1.area_calculator())
+    print()
 
 
     r2 = Rectangle(10, 15)
@@ -49,9 +50,22 @@ def main():
     print(r1 == r2)
     print()
 
+    r3 = Rectangle(10, 10)
+    print(r3)
+    print("Area:", r3.area_calculator())
+    # call the __eq__ method
+    print(r1 == r3)
+    print()
+
+    r4 = Rectangle(5, 30)
+    print(r4)
+    print("Area:", r4.area_calculator())
+    # call the __eq__ method
+    print(r2 == r4)
+    print()
+
     # you can create additional rectangle objects to 
     # test your code or learn more about how the class behaves
-    pass
 
 if __name__ == "__main__":
     main()
